ab8c853e1c27d4b74940489a0eddf04e8cace0a1
narnavg <narnavg@gmail.com>
2023-09-06 14:41:42 +0300

    cart

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-products=["cola","milk","pizza","xl","blue","xxl"]
-
+products=["cola","milk","pizza","xl","blue","xxllllllll"]
+cart=[]
 
 @app.route('/index')
 def index():
-    return render_template("index.html",prods=products)
-
-    
+    cart.append(products[int(request.args.get('selected'))])
+    return render_template("index.html",prods=products,cart=cart)
 
 if __name__ == '__main__':
     app.run(debug=True)
